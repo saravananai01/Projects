061cd12d7fca28fe57ba2196aa21d7db7316b29e
saravananmai01 <saravananmai01@gmail.com>
2024-01-23 13:32:29 +0530

    Added Files

--- C++/constructer overloding/main.cpp
+++ C++/constructer overloding/main.cpp
@@ -2,27 +2,66 @@
 
 using namespace std;
 
-class constructersample
+class Namechecking
 {
 
-public :
+public:
 
-      constructersample()
+      Namechecking()
       {
-          cout << "Welcome To The Class's... ";
+          cout << "\tWelcome To The Class'S... \n";
       }
-      constructersample(string name)
+      Namechecking(string Name)
       {
-          cout << "Welcome To The " << name;
+          cout << "Welcome To The...\n  "<<Name;
+      }
+      Namechecking(string Name,string Initial)
+      {
+          cout << "Welcome To The...\n  "<< Name + Initial;
+      }
+      Namechecking(string Name,string Initial,int Rnum)
+      {
+          cout << "Welcome To The...\n  "<< Name + Initial << Rnum;
+      }
+      void agechecking(string Name,string Initial,int Rnum,int age)
+      {
+          if(age>=20)
+          {
+              cout <<"You Are Eligible\n" <<Name + Initial << Rnum  << age;
+
+          }
+          else
+        {
+          cout <<"You Are Not Eligible\n" <<Name + Initial << Rnum << age;
+          }
       }
 
     };
 
+
 int main()
 {
-    constructersample cse,cse1;
-    string Name;
-    cin >> Name;
-    cse.constructersample(string Name);
+    Namechecking nc;
+    cout << "Enter Your Name... ";
+    string f_Name;
+    cin >> f_Name;
+    string a=f_Name;
+    Namechecking nc1(a);
+    cout << "\nEnter Your Initial... ";
+    string f1_Name;
+    cin >> f1_Name;
+    string a1=f1_Name;
+    Namechecking nc2(a,a1);
+    cout << "\nEnter Your Registernumber... ";
+    int Rnu1;
+    cin >> Rnu1;
+    int a2=Rnu1;
+    Namechecking nc3(a,a1,a2);
+    /*cout <<"Enter Your Age...\n";
+    int age;
+    cin >>age;
+    int a3=age;
+    nc.agechecking(a,a1,a2,a3);
+   */
     return 0;
 }
