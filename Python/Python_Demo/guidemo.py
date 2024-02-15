e563ea9b70f61fbfc2ab0827a598631294083c43
saravananai01 <saravananai01@brightskills.com>
2024-02-15 13:40:44 +0530

    Added Files

--- Python/Python_Demo/guidemo.py
+++ Python/Python_Demo/guidemo.py
@@ -1,33 +1,33 @@
-# from tkinter import *
+from tkinter import *
     
 
-# app=Tk()
-# app.title("My First Python Gui App")
-# app.geometry("500x200")
-# app.config(bg="grey")
+app=Tk()
+app.title("My First Python Gui App")
+app.geometry("500x200")
+app.config(bg="grey")
 
-# def clickresult():
-#     a=10
-#     b=10
-#     c=a+b
-#     ibltittle.config(text=c,)
+def clickresult():
+    a=10
+    b=10
+    c=a+b
+    ibltittle.config(text=c,)
 
-# ibltittle=Label(app,text="Arithmetic Operation")
-# ibltittle.grid(row=0,column=1,padx=40,pady=40)
+ibltittle=Label(app,text="Arithmetic Operation")
+ibltittle.grid(row=0,column=1,padx=40,pady=40)
  
-# inputbox=Entry(app,width=60)
-# inputbox.grid(row=0,column=2)
+inputbox=Entry(app,width=60)
+inputbox.grid(row=0,column=2)
 
-# ibltittle1=Label(app,text="Arithmetic Operation",font="bold",fg="red")
-# ibltittle1.grid(row=1,column=1,padx=40,pady=40)
+ibltittle1=Label(app,text="Arithmetic Operation",font="bold",fg="red")
+ibltittle1.grid(row=1,column=1,padx=40,pady=40)
 
-# inputbox1=Entry(app,width=60)
-# inputbox1.grid(row=1,column=2)
+inputbox1=Entry(app,width=60)
+inputbox1.grid(row=1,column=2)
 
-# clickname=Button(app,text="Addition",command=clickresult,)
-# clickname.grid(row=2,column=2,padx=40,pady=40)
+clickname=Button(app,text="Addition",command=clickresult,)
+clickname.grid(row=2,column=2,padx=40,pady=40)
 
-# app.mainloop()
+app.mainloop()
 
 from tkinter import *
 
