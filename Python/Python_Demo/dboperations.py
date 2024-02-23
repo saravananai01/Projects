7438f1d5e8a33e67a7bfe1368737617e8dcfef4a
saravananai01 <saravananai01@brightskills.com>
2024-02-23 13:37:20 +0530

    Added Files

--- Python/Python_Demo/dboperations.py
+++ Python/Python_Demo/dboperations.py
@@ -1,3 +1,4 @@
+import os
 from tkinter import *
 import tkinter as tk
 import mysql.connector
@@ -36,6 +37,22 @@ def aboutpage():
     lntinfo=tk.Label(abt,text=message)
     lntinfo.pack()
     abt.mainloop()
+
+# Images :
+
+    # titleImageFrame=tk.Frame(details,width=600,height=400, bg="black")
+    # titleImageFrame.pack()
+
+imagedir=os.path.join(os.path.dirname(__file__),'img')
+print("Path Name is :",imagedir)
+
+iamgelocation=os.path.join(imagedir,'Student MarkList.gif')
+print("Image Loaction is :" + iamgelocation)
+
+titleimage=tk.PhotoImage("titleimag",file=os.path.join(imagedir,"Student MarkList.gif"))
+
+lbltitlimg=tk.Label(image=titleimage)
+lbltitlimg.pack()
 
 def insert():
     a=vlinput1.get()
@@ -116,59 +133,59 @@ def clear():
 
 # Name Creating :
 lbltit1=Label(details,text="Name: ",font=("Times New Roman", 10))
-lbltit1.grid(row=1,column=0)
+lbltit1.grid(row=11,column=10)
 lbltit2=Label(details,text="Age: ",font=("Times New Roman", 10))
-lbltit2.grid(row=2,column=0)
+lbltit2.grid(row=12,column=10)
 lbltit3=Label(details,text="Tamil: ",font=("Times New Roman", 10))
-lbltit3.grid(row=3,column=0)
+lbltit3.grid(row=13,column=10)
 lbltit4=Label(details,text="English:",font=("Times New Roman", 10))
-lbltit4.grid(row=4,column=0)
+lbltit4.grid(row=14,column=10)
 lbltit5=Label(details,text="Maths:",font=("Times New Roman", 10))
-lbltit5.grid(row=5,column=0)
+lbltit5.grid(row=15,column=10)
 lbltit6=Label(details,text="Science:",font=("Times New Roman", 10))
-lbltit6.grid(row=6,column=0)
+lbltit6.grid(row=16,column=10)
 lbltit7=Label(details,text="Social Science:",font=("Times New Roman", 10))
-lbltit7.grid(row=7,column=0)
+lbltit7.grid(row=17,column=10)
 lbltit8=Label(details,text="S_no :",font=("Times New Roman",10))
-lbltit8.grid(row=8,column=0)
+lbltit8.grid(row=18,column=10)
 
 # Value Entery :
 vlinput1=Entry(details,width=20)
-vlinput1.grid(row=1,column=2)
+vlinput1.grid(row=11,column=12)
 vlinput2=Entry(details,width=20)
-vlinput2.grid(row=2,column=2)
+vlinput2.grid(row=12,column=12)
 vlinput3=Entry(details,width=20)
-vlinput3.grid(row=3,column=2)
+vlinput3.grid(row=13,column=12)
 vlinput4=Entry(details,width=20)
-vlinput4.grid(row=4,column=2)
+vlinput4.grid(row=14,column=12)
 vlinput5=Entry(details,width=20)
-vlinput5.grid(row=5,column=2)
+vlinput5.grid(row=15,column=12)
 vlinput6=Entry(details,width=20)
-vlinput6.grid(row=6,column=2)
+vlinput6.grid(row=16,column=12)
 vlinput7=Entry(details,width=20)
-vlinput7.grid(row=7,column=2)
+vlinput7.grid(row=17,column=12)
 vlinput8=Entry(details,width=20)
-vlinput8.grid(row=8,column=2)
+vlinput8.grid(row=18,column=12)
 
 # Show OutPut :
 stoutput1=Label(details,text="Your Result :",font=("Times New Roman", 10))
-stoutput1.grid(row=12,column=15)
+stoutput1.grid(row=20,column=15)
 stoutput2=Label(details,text=" ",font=("Times New Roman", 10))
-stoutput2.grid(row=13,column=15)
+stoutput2.grid(row=21,column=15)
 stoutput3=Label(details,text=" ",font=("Times New Roman", 10))
-stoutput3.grid(row=14,column=15)
+stoutput3.grid(row=22,column=15)
 stoutput4=Label(details,text=" ",font=("Times New Roman", 10))
-stoutput4.grid(row=15,column=15)
+stoutput4.grid(row=23,column=15)
 stoutput5=Label(details,text=" ",font=("Times New Roman", 10))
-stoutput5.grid(row=16,column=15)
+stoutput5.grid(row=24,column=15)
 stoutput6=Label(details,text=" ",font=("Times New Roman", 10))
-stoutput6.grid(row=17,column=15)
+stoutput6.grid(row=25,column=15)
 stoutput7=Label(details,text=" ",font=("Times New Roman", 10))
-stoutput7.grid(row=18,column=15)
+stoutput7.grid(row=26,column=15)
 stoutput8=Label(details,text=" ",font=("Times New Roman", 10))
-stoutput8.grid(row=19,column=15)
+stoutput8.grid(row=27,column=15)
 stoutput9=Label(details,text=" ",font=("Times New Roman", 10))
-stoutput9.grid(row=20,column=15)
+stoutput9.grid(row=28,column=15)
 
 # Create Button :
 insbtn=Button(details,text="Insert",font=("Times New Roman", 10),activebackground="blue",activeforeground="red",command=insert)
@@ -187,35 +204,42 @@ clrbtn.grid(row=22,column=25)
 # Menu Bar :
 menubar=tk.Menu(details)
 filemenu=tk.Menu(menubar,tearoff=0)
-menubar.add_cascade(label="File",menu=filemenu,underline=0)
-filemenu.add_command(label="New",underline=0,accelerator="Alt+N")
-filemenu.add_command(label="New Window",underline=4,accelerator="Alt+W")
-filemenu.add_command(label="Open",underline=0,accelerator="Alt+O")
-filemenu.add_command(label="Save",underline=0,accelerator="Alt+S")
-filemenu.add_command(label="Save As",underline=4,accelerator="Alt+A")
+menubar.add_cascade(label="File",menu=filemenu,underline=0,accelerator="Alt+F")
+filemenu.add_command(label="New",underline=0,accelerator="Ctrl+N")
+filemenu.add_command(label="New Window",underline=4,accelerator="Ctrl+W")
+filemenu.add_command(label="Open",underline=0,accelerator="Ctrl+O")
+filemenu.add_command(label="Save",underline=0,accelerator="Ctrl+S")
+filemenu.add_command(label="Save As",underline=4,accelerator="Shift+Ctrl+A")
 filemenu.add_separator()
-filemenu.add_command(label="PageSetup",underline=7,accelerator="Alt+U")
-filemenu.add_command(label="Print",underline=0,accelerator="Alt+P")
+filemenu.add_command(label="PageSetup",underline=7,accelerator="Ctrl+U")
+filemenu.add_command(label="Print",underline=0,accelerator="Ctrl+P")
 filemenu.add_separator()
-filemenu.add_command(label="Exit",underline=1,command=quit,accelerator="Ctr+X")
+filemenu.add_command(label="Exit",underline=1,command=quit,accelerator="Ctrl+X")
+
+# Format Bar :
+formatmenu=tk.Menu(menubar,tearoff=0)
+menubar.add_cascade(label="Format",menu=formatmenu,underline=2,accelerator="Alt+R")
+formatmenu.add_command(label="Word Wrap",underline=0)
+formatmenu.add_command(label="Font...",underline=0)
 
 # Edit Bar :
 editmenu=tk.Menu(menubar, tearoff=0)
-menubar.add_cascade(label="Edit",menu=editmenu)
-editmenu.add_command(label="Undo",  underline=0, accelerator="Ctr+Z")
-editmenu.add_command(label="Redo",  underline=4, accelerator="Ctr+X")
+menubar.add_cascade(label="Edit",menu=editmenu,underline=0,accelerator="Alt+E")
+editmenu.add_command(label="Undo",  underline=0, accelerator="Ctrl+Z")
+editmenu.add_command(label="Redo",  underline=4, accelerator="Ctrl+X")
 editmenu.add_separator()
-editmenu.add_command(label="Copy",  underline=0, accelerator="Ctr+C")
-editmenu.add_command(label="Cut",  underline=0, accelerator="Ctr+X")
-editmenu.add_command(label="Paste",  underline=0, accelerator="Ctr+V")
+editmenu.add_command(label="Copy",  underline=0, accelerator="Ctrl+C")
+editmenu.add_command(label="Cut",  underline=0, accelerator="Ctrl+X")
+editmenu.add_command(label="Paste",  underline=0, accelerator="Ctrl+V")
 
 # View Bar:
-viewmenu=tk.Menu(menubar)
-menubar.add_cascade(label="View")
+viewmenu=tk.Menu(menubar,tearoff=0)
+menubar.add_cascade(label="View",menu=viewmenu,underline=0,accelerator="Alt+V")
+viewmenu.add_command(label="Status Bar")
 
 # Help Bar :
 helpmenu=tk.Menu(menubar,tearoff=0)
-menubar.add_cascade(label="Help",menu=helpmenu)
+menubar.add_cascade(label="Help",menu=helpmenu,underline=0,accelerator="Alt+H")
 helpmenu.add_command(label="About",  underline=0, accelerator="Alt+A", command=aboutpage)
 
 
